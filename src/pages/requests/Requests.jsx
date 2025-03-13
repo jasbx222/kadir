6029cb8da8bb893e8aa5f7f67e166554d1d774ec
jasbx222 <gloop216@gmail.com>
2025-03-13 12:33:40 +0300

    t

--- src/pages/requests/Requests.jsx
+++ src/pages/requests/Requests.jsx
@@ -9,12 +9,12 @@ const Requests = () => {
  
 
 <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+    <table class="w-full text-sm text-left rtl:text-right text-gray-500 ">
         <caption class="p-5 text-lg font-semibold text-left rtl:text-right text-gray-900 bg-white dark:text-white dark:bg-gray-800">
           الطلبات
 
         </caption>
-        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50  dark:text-gray-400">
             <tr>
                 <th scope="col" class="px-6 py-3">
                 اسم مقدم الخدمه
