be3cb61ba09c8e26a5a86bee05865aa5dc9d51be
jasbx222 <gloop216@gmail.com>
2025-03-13 12:53:59 +0300

    tf

--- src/pages/requests/Requests.jsx
+++ src/pages/requests/Requests.jsx
@@ -4,17 +4,18 @@ import { FaPen } from "react-icons/fa";
 import { FaRegEye } from "react-icons/fa";
 import { Link } from 'react-router-dom';
 import { requestData } from './requestData';
+import Pageinition from '../../componentes/pageination/Pageinition';
 const Requests = () => {
   return (
  
 
 <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
     <table class="w-full text-sm text-left rtl:text-right text-gray-500 ">
-        <caption class="p-5 text-lg font-semibold text-left rtl:text-right text-gray-900 bg-white dark:text-white dark:bg-gray-800">
+        <caption class="p-5 text-lg font-semibold text-left rtl:text-right text-gray-900 bg-white  ">
           الطلبات
 
         </caption>
-        <thead class="text-xs text-gray-700 uppercase bg-gray-50  dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50  ">
             <tr>
                 <th scope="col" class="px-6 py-3">
                 اسم مقدم الخدمه
@@ -34,8 +35,8 @@ const Requests = () => {
         <tbody>
         {
             requestData.map((r)=>(
-                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                <tr class="bg-white border-b  border-gray-200">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap ">
                   {r.service_provider_name}
                 </th>
                 <td class="px-6 py-4">
@@ -46,9 +47,9 @@ const Requests = () => {
                 </td>
             
                 <td class="px-6 py-4  flex justify-between items-center gap-5">
-                    <Link to={`/requestid/${r.id}`} class="font-medium text-blue-600 dark:text-blue-500 hover:underline"><FaPen size={25}/></Link>
-                    <Link href="#" class="font-medium text-lime-400 dark:text-blue-500 hover:underline"><FaRegEye size={25}/></Link>
-                    <Link href="#" class="font-medium text-red-500 dark:text-blue-500 hover:underline"><MdDelete size={25}/></Link>
+                    <Link to={`/requestid/${r.id}`} class="font-medium text-blue-600  hover:underline"><FaPen size={25}/></Link>
+                    <Link href="#" class="font-medium text-lime-400  hover:underline"><FaRegEye size={25}/></Link>
+                    <Link href="#" class="font-medium text-red-500 hover:underline"><MdDelete size={25}/></Link>
                
                 </td>
             </tr>
@@ -58,6 +59,7 @@ const Requests = () => {
         
         </tbody>
     </table>
+   <div className='flex justify-center items-center mt-5 mb-5'> <Pageinition/></div>
 </div>
 
   )
