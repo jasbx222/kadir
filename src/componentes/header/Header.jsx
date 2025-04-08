513f7f3d4bc9809a3c3b3ea959a172947dd3795a
jasbx222 <gloop216@gmail.com>
2025-04-08 11:57:21 +0300

    SDfssfdff

--- src/componentes/header/Header.jsx
+++ src/componentes/header/Header.jsx
@@ -1,10 +1,11 @@
 import Notification from "./Notification";
+import './Header.css';
 const Header = () => {
   return (
-    <div className="flex justify-between items-center    p-4 bg-white  w-full">
+    <div className="flex justify-between items-center     p-4 bg-white  w-full">
       <div className="hidden md:flex">{""}</div>
 
-      <div className=" relative left-80 top-24 md:top-0 md:mr-10 md:static md:left-0">
+      <div className=" relative notif left-80 top-24 md:top-0 md:mr-10 md:static md:left-0">
         <Notification />
       </div>
     </div>
